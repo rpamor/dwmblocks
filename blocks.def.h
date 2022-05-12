c02770e20217b09f70616c74d9eaaeb54acc918f
Richard Pamor <richard.pamor@yahoo.com>
2022-05-12 17:05:03 +0800

    blocks.def.h: modify blocks[]

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "sb-pacman-packages.sh", 0, 8},
+	{"", "sb-battery.sh", 1, 0},
+	{"", "sb-rss.sh", 0, 6},
+	{"", "sb-volume.sh", 1, 10},
+	{"", "sb-network.sh", 5, 4},
+	{"", "sb-clock.sh", 60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
